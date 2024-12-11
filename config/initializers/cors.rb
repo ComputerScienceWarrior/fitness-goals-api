61c1987f46a1c278207d3fabef78dc615cbeba9d
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-11 12:40:59 -0700

    updates cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "exp://192.168.12.175:8081"
+    origins "exp://10.0.2.2:8081", 'http://localhost:8081', 'http://10.0.2.2:8081'
 
     resource "*",
       headers: :any,
