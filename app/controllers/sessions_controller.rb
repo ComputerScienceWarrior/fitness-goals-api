b56fb430dd7101eb974d2ba0d8415e940a5cdb26
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-12 08:24:39 -0700

    sets up jwt token and method to geenrate it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,22 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     
-    if user&.authenticate(params[:password]) # Assumes you're using has_secure_password
-      session[:user_id] = user.id
-      render json: { message: 'Login successful' }, status: :ok
+    if user&.authenticate(params[:password])
+      token = generate_jwt(user)
+      render json: { token: token }, status: :ok
     else
       render json: { message: 'Invalid credentials' }, status: :unauthorized
     end
   end
+
+  private 
+
+  def generate_jwt(user)
+    payload = { user_id: user.id }
+    secret = Rails.application.secret_key_base
+    expiration = 24.hours.from_now.to_i
+
+    JWT.encode(payload.merge(exp: expiration), secret, 'HS256')
+  end
+
 end
